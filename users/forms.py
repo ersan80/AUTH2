bc001ba768f69cb94b6f13bdaefaeaf29a4823b1
ersan80 <ersan.kuleli80@gmail.com>
2022-05-30 14:19:42 +0300

    django auth

---
+++ users/forms.py
@@ -0,0 +1,18 @@
+from dataclasses import field, fields
+from pyexpat import model
+from django.contrib.auth.models import User
+from .models import UserProfile
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ("user",)
+        
---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from django.urls import path
+from .views import register, user_login, user_logout
+
+urlpatterns = [
+    path("logout/", user_logout,name="logout"),
+    path("register/", register,name="register"),
+    path("login/", user_login,name="login"),
+    
+    
+]
